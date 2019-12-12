a1fda24cf060a475ebb02777e8e250d4127b792c
Michael <facilitators@softstackfactory.com>
2019-12-11 23:30:26 -0800

    production build, added different components to login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private authGuard: AuthGuardService, private router: Router) { }
+  constructor(public authGuard: AuthGuardService, public router: Router) { }
   mustLogIn: boolean
 
   ngOnInit() {
