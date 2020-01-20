75f53712dce4a6e9cfb610827e5d2c5effb55c63
Michael <michaelpp2001@gmail.com>
2020-01-19 23:04:58 -0800

    added an ion chip, and delete functionality with an alert with comfirmation

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { Storage } from '@ionic/storage';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,34 @@ export class PhotoServiceService {
 
   constructor(
     private camera: Camera,
-    private storage: Storage) { }
+    private storage: Storage,
+    public alertController: AlertController
+    ) { }
+
+    async presentDeleteConfirm(photo) {
+      const alert = await this.alertController.create({
+        header: 'Confirm!',
+        message: 'Are you sure you want to <strong>Delete</strong>?',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: (blah) => {
+              console.log('Confirm Cancel: blah');
+            }
+          }, {
+            text: 'Delete',
+            handler: () => {
+              let result = this.photos.splice( this.photos.findIndex(find => find.data === photo.data), 1);
+              console.log("deleted")
+              this.set()
+            }
+          }
+        ]
+      });
+      await alert.present();
+    }
 
     takePicture() {
       const options: CameraOptions = {
@@ -34,16 +62,21 @@ export class PhotoServiceService {
           data: 'data:image/jpeg;base64,' + imageData
         });
         // Save all photos for later viewing
-        this.storage.set('photos', this.photos);
+        this.set();
       }, (err) => {
         // Handle error
         console.log("Camera issue:" + err);
       });
     }
 
+    //resets storage key and value of photos
+    set() {
+      this.storage.set('photos', this.photos);
+    }
+
     loadSaved() {
       this.storage.get('photos').then((photos) => {
-        this.photos = photos || [];
+      this.photos = photos || [];
       });
     }
 }
