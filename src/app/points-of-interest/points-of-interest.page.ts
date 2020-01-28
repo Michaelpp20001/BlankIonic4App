00f2a29490d451acca5fa819b9ec695f3c77c75e
Michael <michaelpp2001@gmail.com>
2020-01-28 01:38:46 -0800

    added a map to points of interest page

--- src/app/points-of-interest/points-of-interest.page.ts
+++ src/app/points-of-interest/points-of-interest.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MapService } from '../services/map.service';
+
+declare var google;
 
 @Component({
   selector: 'app-points-of-interest',
@@ -8,11 +10,29 @@ import { MapService } from '../services/map.service';
 })
 export class PointsOfInterestPage implements OnInit {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any
+
   constructor(
     private mapService: MapService
-  ) { }
-
+  ) {}
+  
   ngOnInit() {
+    this.loadMap()
+  }
+
+  loadMap(){
+
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
   }
 
 }
--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -21,4 +21,6 @@ export class MapService {
      console.log('Error getting location', error);
    });
   }
+
+  
 }
